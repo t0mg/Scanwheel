b05bdd1b30fa24e3ca2dcf842456301cfed04292
James <james@ancient-workshop.com>
2026-07-05 14:15:28 +1200

    startup ramp

--- scanwheel.py
+++ scanwheel.py
@@ -15,6 +15,7 @@ def bytearray_aligned(size, alignment):
     aligned_addr = (uctypes.addressof(buffer) + alignment) & ~(alignment - 1)
     return uctypes.bytearray_at(aligned_addr, size)
 
+
 def pio_assemble(pclk):
     assert(pclk >= 2)
     
@@ -26,15 +27,15 @@ def pio_assemble(pclk):
         out(y, 32)                          # load the loop counter
         jmp(not_y, "at_speed")              # if it's zero, proceed to scanning
         
-        mov(x, y).side(0)       [pclk-2]    # step pin low
-        label("spin_lo")
-        nop()                   [pclk-2]
-        jmp(x_dec, "spin_lo")
-        
-        mov(x, y).side(1)       [pclk-1]    # step pin high
+        mov(x, y).side(1)       [pclk-2]    # step pin high
         label("spin_hi")
         nop()                   [pclk-2]
         jmp(x_dec, "spin_hi")
+        
+        mov(x, y).side(0)       [pclk-1]    # step pin low
+        label("spin_lo")
+        nop()                   [pclk-2]
+        jmp(x_dec, "spin_lo")
         
         jmp("spin_up")
         
@@ -43,17 +44,17 @@ def pio_assemble(pclk):
         
         wrap_target()                       # main loop
         
-        out(pins, 8).side(0)    [pclk-2]    # pixel output...
+        out(pins, 8).side(1)    [pclk-2]    # pixel output...
         mov(x, y)                           # ... +1 clock to load the loop counter
-        label("step_lo")                    # output half a line with the step pin low:
-        out(pins, 8).side(0)    [pclk-2]    #   pixel output...
-        jmp(x_dec, "step_lo")               #   ... +1 clock to loop
+        label("step_hi")                    # output half a line with the step pin high:
+        out(pins, 8).side(1)    [pclk-2]    #   pixel output...
+        jmp(x_dec, "step_hi")               #   ... +1 clock to loop
         
-        out(pins, 8).side(1)    [pclk-2]    # same again, with the step pin high
+        out(pins, 8).side(0)    [pclk-2]    # same again, with the step pin low
         mov(x, y)
-        label("step_hi")
-        out(pins, 8).side(1)    [pclk-2]
-        jmp(x_dec, "step_hi")
+        label("step_lo")
+        out(pins, 8).side(0)    [pclk-2]
+        jmp(x_dec, "step_lo")
         
         wrap()
         
@@ -78,15 +79,15 @@ class ScanWheel:
         self.pin_enable = enable
         self.pin_reset = reset
 
+        chunk_count, linewidth = ScanWheel.calculate_chunk_count(linewidth, scanlines)
+        chunk_size = (linewidth * scanlines) // chunk_count
+        
         self.frame_w = int(linewidth)
         self.frame_h = int(scanlines)
         self.frame_rate = framerate
         
         self.frame_size = int(self.frame_w * self.frame_h)
-        
-        chunk_count = self.enforce_chunk_count()            
-        chunk_size = self.frame_size // chunk_count
-        
+
         self.framebuffer = bytearray_aligned(self.frame_size, chunk_size)
         
         self.sm = self.state_machine()
@@ -103,7 +104,7 @@ class ScanWheel:
         # turn on the driver
         machine.Pin(self.pin_enable, machine.Pin.OUT).value(0)
         machine.Pin(self.pin_reset, machine.Pin.OUT).value(1)
-        time.sleep_us(10000)
+        time.sleep_us(1000)
         
         # build the preamble list
         preamble = array.array('I')
@@ -114,14 +115,28 @@ class ScanWheel:
         # set a series of decreasing scanline lengths to ramp up the motor
         start_w = self.frame_w * self.frame_rate / 2
 
-        valign = round(self.valign * self.frame_h)
-        tsteps = 3 * self.frame_rate * self.frame_rate
-        for t in range(tsteps):
-            ramp = 1 - math.pow(1 - t / tsteps, 8)
-            preamble.append(int(lerp(start_w, self.frame_w, ramp)) // 2 - 2)
+        valign = self.valign
+        if self.halign < 0:
+            valign += 1
+        halign = round(self.halign * self.frame_w) % self.frame_w
+        
+        a = -0.4
+        b = 0.8
+        f = 1
+        fw = self.frame_rate * self.frame_w * b
+        while True:
+            freq = pow(f, a)
+            w = fw * freq
+            if w < self.frame_w:
+                break
+            
+            preamble.append(int(w) // 2 - 2)
             valign += 1
             
-        while (valign % self.frame_h) != 0:
+            f += b
+
+            
+        for _ in range((self.frame_h * 2) - (valign % self.frame_h)):
             preamble.append(int(self.frame_w) // 2 - 2)
             valign += 1
         
@@ -132,23 +147,24 @@ class ScanWheel:
         preamble.append(int(self.frame_w) // 2 - 2)
         
         # pad with dummy pixels to set the horizontal alignment
-        halign = round(self.halign * self.frame_w) % self.frame_w
         for _ in range(halign):
             preamble.append(int(0))
             
+        print(len(preamble))
+            
         # now send that to the state machine
         self.sm.put(preamble)
-        
+
         # and hand over to the dma chain
         self.dma[0].active(1)
         
     def align(self):
         machine.Pin(self.pin_enable, machine.Pin.OUT).value(0)
-        time.sleep_us(10000)
+        time.sleep_us(1000)
         machine.Pin(self.pin_reset, machine.Pin.OUT).value(0)
-        time.sleep_us(10000)
+        time.sleep_us(1000)
         machine.Pin(self.pin_reset, machine.Pin.OUT).value(1)
-        time.sleep_us(10000)
+        time.sleep_us(1000)
 
         
         green = machine.Pin(self.pin_leds + 1, machine.Pin.OUT)
@@ -182,36 +198,41 @@ class ScanWheel:
                         k = k.strip()
                         
                         if   k == 'halign':
-                            self.halign = float(v)
+                            self.halign = int(v)
                         elif k == 'valign':
-                            self.valign = float(v)
+                            self.valign = int(v)
 
         except:
             pass
 
-
-    def enforce_chunk_count(self):
-        chunk_count = max(2, self.frame_size // max_factor(self.frame_size))
-        
+    @staticmethod
+    def calculate_chunk_count(frame_w, frame_h):
         DMA_CHANNEL_COUNT = 12
+        
+        frame_size = frame_w * frame_h
+        chunk_count = frame_size // max_factor(frame_size)
         
         if chunk_count >= DMA_CHANNEL_COUNT:
             new_w = 0
             for c in range(2, DMA_CHANNEL_COUNT + 1):
                 for b in range(2):
-                    s = 1 << max(0, bit_length((self.frame_size - 1) // c) + b)
-                    w = s * c / self.frame_h
-                    if w%1 == 0:
-                        if abs(w - self.frame_w) < abs(new_w - self.frame_w):
+                    s = 1 << max(0, bit_length((frame_size - 1) // c) + b)
+                    w = s * c / frame_h
+                    if w % 1 == 0:
+                        if abs(w - frame_w) < abs(new_w - frame_w):
                             new_w = w
 
-            self.frame_w = int(new_w)            
-            self.frame_size = int(self.frame_w * self.frame_h)
-            chunk_count = max(2, self.frame_size // max_factor(self.frame_size))
+            frame_w = new_w
             
-            print(f'frame dimensions are unachievable - using {self.frame_w}x{self.frame_h} instead')
+            frame_size = frame_w * frame_h
+            chunk_count = frame_size // max_factor(frame_size)
+            
+            print(f'frame dimensions are unachievable - using {frame_w}x{frame_h} instead')
         
-        return chunk_count
+        while chunk_count * 2 <= DMA_CHANNEL_COUNT:
+            chunk_count *= 2
+        
+        return chunk_count, frame_w
 
     @staticmethod
     def framerate_params(target_fps, frame_size):
@@ -244,6 +265,7 @@ class ScanWheel:
     
     @staticmethod
     def dma_chain(frame_buffer, chunk_count, chunk_size, sm):
+        print(f'creating dma chain with {chunk_count} chunks of {chunk_size} bytes')
         dma = [rp2.DMA() for _ in range(chunk_count)]
             
         chunk_bits = bit_length(chunk_size)
@@ -262,13 +284,13 @@ class ScanWheel:
 
 
 if __name__ == "__main__":
-    sw = ScanWheel(linewidth=2048, framerate=15)
+    sw = ScanWheel(linewidth=2048, framerate=24)
     
     try:
         sw.align()
             
         sw.start(leds_state=0b00000111)
-
+        
         with open('tcf2048.raw', 'rb') as f:
             f.readinto(sw.framebuffer)
 
